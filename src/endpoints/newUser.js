a7473f15ba924a108315f21601642213341b7159
Ratul Saha <thatanonyg@gmail.com>
2021-02-15 12:17:40 +0530

    Episode 4

--- src/endpoints/newUser.js
+++ src/endpoints/newUser.js
@@ -1,18 +1,34 @@
 const Router = require("express-promise-router");
 const { UserModel } = require("../models/UserModel");
+const { hash } = require("bcrypt");
+const Constants = require("../utils/constants");
 const route = Router();
 
 module.exports = route;
 
-route.post("/new-user", (req, res) => {
-	const body = req.body;
+route.post("/new-user", async (req, res) => {
+	const { username, password } = req.body;
+
+	const hashedPassword = await hash(password, 10);
 
 	const newUser = new UserModel({
-		id: body.id,
-		username: body.username,
-		password: body.password,
+		id: genId(),
+		username: username,
+		password: hashedPassword,
 	});
 	newUser.save().catch(console.error);
 
-	res.send("OK");
+	res.status(Constants.HTTPCodes.Created).send({
+		message: "Generated profile successfully",
+		user: { username, id: newUser.id },
+	});
 });
+
+function genId() {
+	const useChars =
+		"qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890.-_";
+	return Array.from(
+		{ length: 16 },
+		() => useChars[Math.floor(Math.random() * useChars.length)]
+	).join("");
+}
---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+const Constants = {
+	HTTPCodes: {
+		Success: 200,
+		Created: 201,
+	},
+};
+
+Object.freeze(Constants);
+
+module.exports = Constants;
